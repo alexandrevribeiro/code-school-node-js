7feeb58f418d72bcfbf5dc20c6942bfe0e7fa949
Alexandre Ribeiro <Alexandre Ribeiro>
2017-04-05 00:21:48 -0300

    Another small change

--- node-js-course/level-3-streams/streams.js
+++ node-js-course/level-3-streams/streams.js
@@ -5,7 +5,7 @@ var http = require('http');
 http.createServer(function(request, response) {
     response.writeHead(200);
 
-    request.on('readble', function() {
+    request.on('readable', function() {
       var chunk = null;
       while ((chunk = request.read()) !== null) {
         response.write(chunk);
