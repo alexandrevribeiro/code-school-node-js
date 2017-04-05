f6c8d1280f16eb2c7386e70a1e5daad29fbba734
Alexandre Ribeiro <Alexandre Ribeiro>
2017-04-04 23:03:21 -0300

    streams

---
+++ node-js-course/streams.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+// "request" and "response" and streams and EventEmitters
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+
+    request.on('readble', function() {
+      var chunk = null;
+      while ((chunk = request.read()) != null) {
+        response.write(chunk);
+      }
+    });
+
+    request.on('end', function() {
+      response.end();
+    });
+
+    // The event listeners created above can just be replaced by:
+    // request.pipe(response);
+    
+}).listen(8080);
