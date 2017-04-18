44e21ae495a127a968cb32e36ea84ccdc19547b0
Alexandre Ribeiro <Alexandre Ribeiro>
2017-04-18 00:09:30 -0300

    using template (ejs)

--- node-js-course/level-5-express/first-app/modules/github-api.js
+++ node-js-course/level-5-express/first-app/modules/github-api.js
@@ -1,36 +1,42 @@
 var request = require('request');
 var url = require('url');
+var githubAcc = require('./github-account');
 
-var githubHost = 'api.github.com';
+var githubHost = githubAcc.username + ':' + githubAcc.password + '@api.github.com';
 
-var reqOptions = {
-    headers: {
-        'User-Agent': 'vribeiro-alexandre'
-    },
-    json: true
+var callGithubApi = function (pathname, callback) {
+
+    var reqOptions = {
+        json: true,
+        headers: { 'User-Agent': 'vribeiro-alexandre' }
+    };
+
+    var urlOptions = {
+        protocol: 'https',
+        host: githubHost,
+        pathname: pathname
+    };
+
+    var githubUrl = url.format(urlOptions);
+    console.log('Requesting... ' + githubUrl);
+
+    request(githubUrl, reqOptions, function (err, res, jsonObj) {
+        callback(err, jsonObj);
+    });
 };
 
 var githubApi = {
-    getUser: function(username, callback) {
-
-        var urlOptions = {
-            protocol: 'https',
-            host: githubHost,
-            pathname: '/users/' + username
-        };
-
-        var githubUrl = url.format(urlOptions);
-
-        request(githubUrl, reqOptions, function(err, res, body) {
-            
-            if (err)
-                callback(err);
-            else {
-                callback(null, body);
-            }            
-        });
+    getUser: function (username, callback) {
+        var pathname = '/users/' + username;
+        callGithubApi(pathname, callback);
+    },
+    getRepos: function (username, callback) {
+        var pathname = '/users/' + username + '/repos';
+        callGithubApi(pathname, callback);
     }
 };
+
+
 
 
 module.exports = githubApi;
