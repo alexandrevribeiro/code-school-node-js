40c374f4a055cfe7a2a29369dd434a72b5d28ed7
Alexandre Ribeiro <Alexandre Ribeiro>
2017-04-17 22:06:28 -0300

    express - routing

--- node-js-course/level-5-express/first-app/app.js
+++ node-js-course/level-5-express/first-app/app.js
@@ -1,8 +1,32 @@
 var express = require('express');
+var request = require('request');
+var url = require('url');
+
 var app = express();
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/github/users/:username/json', function(req, response) {
+
+    var username = req.params.username;
+
+    var urlOptions = {
+        protocol: 'https',
+        host: 'api.github.com',
+        pathname: '/users/' + username
+    };
+
+    var reqOptions = {
+        headers: {
+            'User-Agent': 'vribeiro-alexandre'
+        }
+    };
+
+    var githubUrl = url.format(urlOptions);
+    request(githubUrl, reqOptions).pipe(response);
 });
 
 app.listen(8080);
