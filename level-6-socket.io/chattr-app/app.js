01f1f132889b7db0fb0d1424a683e6875ae62226
Alexandre Ribeiro <Alexandre Ribeiro>
2017-04-20 21:05:18 -0300

    Using redis

--- level-6-socket.io/chattr-app/app.js
+++ level-6-socket.io/chattr-app/app.js
@@ -1,10 +1,20 @@
 var app = require('express')();
 var httpServer = require('http').Server(app);
 var io = require('socket.io')(httpServer);
+var redisClient = require('redis').createClient();
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
+
+var storeMessage = function(name, message) {
+    var messageObj = JSON.stringify({name: name, message: message});
+
+    redisClient.lpush('messages', messageObj, function(err, response) {
+        // Keeps only the newest 10 items
+        redisClient.ltrim('messages', 0, 9);
+    });
+};
 
 io.on('connection', function(clientSocket) {
     console.log('Client connected...');
@@ -14,6 +24,18 @@ io.on('connection', function(clientSocket) {
         clientSocket.nickname = name;
         clientSocket.broadcast.emit('join', name);
         console.log(name + ' joined!');
+
+        // Gets the stored messages and emit the 'chat-message' for each of them.
+        // The '0, -1' means you want to get from the first item (0) to the last one (-1)
+        redisClient.lrange('messages', 0, -1, function(err, messages) {
+            // Reverse so they are emitted in correct order
+            messages.reverse();
+
+            messages.forEach(function(message) {
+                var msgObj = JSON.parse(message);
+                clientSocket.emit('chat-message', msgObj.name + ': ' + msgObj.message);
+            });
+        });
     });
 
     // Listen for 'messages' events
@@ -25,7 +47,10 @@ io.on('connection', function(clientSocket) {
         clientSocket.broadcast.emit('chat-message', message);
 
         // Then sends the same message back to our socket
-        clientSocket.emit('chat-message', message);        
+        clientSocket.emit('chat-message', message);  
+
+        // Stores the message
+        storeMessage(clientSocket.nickname, msg);
     });
 });
 
